2644282be88dc1599b4ac173e7172a8e16d2335b
delens <delens@science.ru.nl>
2021-12-16 10:36:41 +0000

    works now

--- rpi-rgb-led-matrix/examples-api-use/demo-main.cc
+++ rpi-rgb-led-matrix/examples-api-use/demo-main.cc
@@ -185,26 +185,7 @@ public:
     const float deg_to_rad = 2 * 3.14159265 / 360;
     int rotation = 0;
     while (running() && !interrupt_received) {
-      ++rotation;
-      usleep(15 * 1000);
-      rotation %= 360;
-      for (int x = min_rotate; x < max_rotate; ++x) {
-        for (int y = min_rotate; y < max_rotate; ++y) {
-          float rot_x, rot_y;
-          Rotate(x - cent_x, y - cent_x,
-                 deg_to_rad * rotation, &rot_x, &rot_y);
-          if (x >= min_display && x < max_display &&
-              y >= min_display && y < max_display) { // within display square
-            canvas()->SetPixel(rot_x + cent_x, rot_y + cent_y,
-                               scale_col(x, min_display, max_display),
-                               255 - scale_col(y, min_display, max_display),
-                               scale_col(y, min_display, max_display));
-          } else {
-            // black frame.
-            canvas()->SetPixel(rot_x + cent_x, rot_y + cent_y, 0, 0, 0);
-          }
-        }
-      }
+    	canvas()->SetPixel(44,44,0,0,255);
     }
   }
 
