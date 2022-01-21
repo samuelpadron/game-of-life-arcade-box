4c051dbb3115434b4bc6ae1cde46355656f45fc9
Samuel Padron <s.padronalcala@student.science.ru.nl>
2022-01-22 00:05:15 +0100

    destroyed all JNI files :)

--- jni-test/HelloWorldJNI.cpp
+++
@@ -1,7 +0,0 @@
-#include "HelloWorldJNI.h"
-#include <iostream>
-
-JNIEXPORT void JNICALL Java_HelloWorldJNI_sayHello
-	(JNIEnv* env, jobject thisObject) {
-	std::cout << "Hello from C++ :D!" << std::endl;
-	}
--- jni-test/UserData.java
+++
@@ -1,9 +0,0 @@
-public class UserData {
-
-	public String name;
-	public double balance;
-
-	public String getUserInfo() {
-		return "[name]=" + name + ", [balance]=" + balance;
-	}
-}
